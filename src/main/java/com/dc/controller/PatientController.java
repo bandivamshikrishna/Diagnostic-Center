55746c6ef753e4342b72a31b139406c041d40baf
vamshi krishna <kvamshi340@gmail.com>
2026-02-01 00:18:13 +0400

    updated new git remote

--- src/main/java/com/dc/controller/PatientController.java
+++ src/main/java/com/dc/controller/PatientController.java
@@ -12,7 +12,6 @@ import jakarta.validation.Validator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
